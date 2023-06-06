7a84463a0ceaea77f2bba43fac95e1a1d67f808e
vuongnguyen234 <135314992+vuongnguyen234@users.noreply.github.com>
2023-06-07 00:00:40 +0700

    psh

---
+++ ss06_javascript/practice/thuchanh1.js
@@ -0,0 +1,10 @@
+function showMessage(){
+    alert('Xin chào!');
+}
+
+function (Xinchao){
+    document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+    function showMessage(){
+        document.write('Xin chào!');
+    }
+}
